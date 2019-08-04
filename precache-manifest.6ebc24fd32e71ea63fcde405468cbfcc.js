9854e465688766f0804e8e69ccd3c25edeb764d0
Francis Borbe <borbefg@gmail.com>
2019-08-04 10:42:40 +0800

    Updates

---
+++ precache-manifest.6ebc24fd32e71ea63fcde405468cbfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cda376194a0cbea184cba9afdc6c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc0056cf417004d2383",
+    "url": "/static/css/2.aa9cd695.chunk.css"
+  },
+  {
+    "revision": "4f6cec7d6511627f1207",
+    "url": "/static/css/main.8decdce3.chunk.css"
+  },
+  {
+    "revision": "ddc0056cf417004d2383",
+    "url": "/static/js/2.b8d31b06.chunk.js"
+  },
+  {
+    "revision": "4f6cec7d6511627f1207",
+    "url": "/static/js/main.6e0567e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
